85aba02165bf17a29ea891b16cbe9172b3764fad
Iwan Phillips <iwanthomphil@gmail.com>
2020-05-16 17:51:26 +0100

    Temp Images Added for testing.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js/app.js')
     .sass('resources/sass/app.scss', 'public/css/app.css')
+    .copyDirectory('storage/images', 'public/images')
     .copyDirectory('resources/img', 'public/images');
